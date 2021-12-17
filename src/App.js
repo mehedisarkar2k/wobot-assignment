f2f5402a20919e6eecb1693e3a40d144964972f5
Mehedi Hasan Sarkar <mehedisarkar@hotmail.com>
2021-12-17 15:15:25 +0600

    add linkt o the logo

--- src/App.js
+++ src/App.js
@@ -7,9 +7,11 @@ import Login from "./components/Login";
 function App() {
   const [proceed, setProceed] = useState(false);
   return (
-    <div className="mainBg flex items-center justify-center min-h-screen relative">
+    <div className="mainBg flex items-center justify-center min-h-screen relative bg-gray-50">
       <div className="hidden md:block absolute top-6 left-10">
-        <img src={Logo} alt="Company Logo" />
+        <a href="/">
+          <img src={Logo} alt="Company Logo" />
+        </a>
       </div>
 
       {!proceed ? <BasicForm proceedHandler={setProceed} /> : <Login />}
